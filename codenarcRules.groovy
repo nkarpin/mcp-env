694eb83019504610653355d001308aa0c1b1637f
Sergey Galkin <sgalkin@mirantis.com>
2018-05-22 16:19:30 +0400

    Add codenarc rules
    Change-Id: I428bb868a29b12fa827fd910326bdfaeadbc11cf
    Related-Bug: PROD-19951

---
+++ codenarcRules.groovy
@@ -0,0 +1,13 @@
+ruleset {
+  ruleset('rulesets/basic.xml') {
+    exclude 'DuplicateStringLiteral'
+    exclude 'TrailingComma'
+    exclude 'SpaceAroundOperator'
+    exclude 'UnnecessaryGString'
+    exclude 'CyclomaticComplexity'
+    exclude 'CouldBeSwitchStatement'
+    exclude 'SpaceAfterIf'
+    exclude 'UnnecessaryBooleanInstantiation'
+    exclude 'InvertedIfElse'
+  }
+}
