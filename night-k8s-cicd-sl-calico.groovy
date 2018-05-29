71ffca28f7af9ce874b77712a7b93d956fe3488c
Sergey <sgalkin@mirantis.com>
2018-05-29 07:56:02 +0400

    Fix in nightly jobs
    Change-Id: I706a590bca8a448c2d2cece0231c50cba547ee49
    Realted-bug: PROD-19989

--- night-k8s-cicd-sl-calico.groovy
+++ night-k8s-cicd-sl-calico.groovy
@@ -3,7 +3,7 @@ node ('python'){
     build(
           job: 'create-mcp-env',
           parameters: [
-              string(name: 'STACK_NAME', value: 'core-k8s-cicd-sl-calico'),
+              string(name: 'STACK_NAME', value: 'night-core-k8s-cicd-sl-calico'),
               string(name: 'OS_PROJECT_NAME', value: 'mcp-scale-dev'),
               string(name: 'STACK_INSTALL', value: 'core,k8s,stacklight'),
           ],
